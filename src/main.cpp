aa01d0d283811131f530d50ade602601be2ad5c8
Hiroaki Ono <hiono@student.42adel.org.au>
2024-09-03 17:17:31 +0930

    handle up to 4 clients

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,32 @@
 #include <iostream>
 #include <netinet/in.h>
 
+void	printPollfd(struct pollfd fd[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		std::cout << "index: " << i << std::endl;
+		std::cout << "fd: " << fd[i].fd << std::endl;
+		std::cout << "events: " << fd[i].events << std::endl;
+		std::cout << "revents: " << fd[i].revents << std::endl;
+	}
+}
+
 int main()
 {
     // creating socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	// convert socket non-blocking
+	fcntl(serverSocket, F_SETFL, O_NONBLOCK);
 
     // specifying the address
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-	struct sockaddr	*pServerAddress = reinterpret_cast<struct sockaddr *>(&serverAddress);
 
     // binding socket.
-    if (bind(serverSocket, pServerAddress, sizeof(serverAddress)) == -1)
+    if (bind(serverSocket, reinterpret_cast<struct sockaddr *>(&serverAddress), sizeof(serverAddress)) == -1)
 	{
 		perror("bind");
 		return 1;
@@ -32,20 +44,60 @@ int main()
 		return 1;
 	}
 
-    // accepting connection request
-    int clientSocket = accept(serverSocket, nullptr, nullptr);
+	struct pollfd	pfds[5];
+	pfds[0].fd = serverSocket;
+	pfds[0].events = POLLIN;
+	int	fd_count = 1;
 
-    // recieving data
-    char buffer[1024] = { 0 };
 	while (true)
 	{
-		memset(buffer, 0, 1024);
-		recv(clientSocket, buffer, sizeof(buffer), 0);
-		std::cout << "Message from client: " << buffer << std::endl;
-		if (!strncmp(buffer, "EOF", 4))
+		int	events = poll(pfds, fd_count, 20000);
+		if (events == 0)
+		{
+			std::cout << "Poll timed out" << std::endl;
 			break ;
+		}
+		for (int i = 0; i < fd_count; i++)
+		{
+			if (pfds[i].revents != POLLIN)
+				continue ;
+			// accepting connection request
+			if (pfds[i].fd == serverSocket)
+			{
+				if (fd_count == 5)
+				{
+					std::cout << "already 4 clients connected" << std::endl;
+					break ;
+				}
+				std::cout << "Client " << fd_count << " is accepted" << std::endl;
+				int clientSocket = accept(serverSocket, nullptr, nullptr);
+				if (clientSocket == -1)
+				{
+					perror("accept");
+					break ;
+				}
+				pfds[fd_count].fd = clientSocket;
+				pfds[fd_count].events = POLLIN;
+				fd_count++;
+			}
+			// recieving data
+			else
+			{
+				char buffer[1024] = { 0 };
+				if (recv(pfds[i].fd, buffer, sizeof(buffer), 0) == -1)
+				{
+					perror("recv");
+					return 1;
+				}
+				std::cout << "Message from client " << i << " :" << buffer << std::endl;
+				if (!strncmp(buffer, "close", 5))
+				{
+					close(serverSocket);
+					return 0;
+				}
+			}
+		}
 	}
-
     // closing the socket.
     close(serverSocket);
 
